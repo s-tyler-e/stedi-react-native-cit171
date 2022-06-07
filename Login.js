548aa0f1abe42876ec93f8283bddb28f4b1355f7
Tyler Eich <teich6789@gmail.com>
2022-06-07 13:45:19 -0600

    update

---
+++ Login.js
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder = "123-456-7890"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={number}
+        placeholder="1234"
+        keyboardType="numeric"
+        secureTextEntry={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
